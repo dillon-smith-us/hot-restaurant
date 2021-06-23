8cefc7789ba4ccbd969d40762f1861af5e0d69f5
Dillon Smith <dillsmth@gmail.com>
2021-06-22 19:45:45 -0500

    added folders, setting apiroute and htmlroute

--- server.js
+++ server.js
@@ -5,3 +5,17 @@ const path = require("path");
 
 const app = express()
 const PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ROUTER
+// The below points us to a series of "route" files.
+// These routes give the server a "map" of how to respond when users visit or request data from various URLs.
+
+require("./routes/apiRoutes")(app)
+require("./routes/htmlRoutes")(app);
+
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
